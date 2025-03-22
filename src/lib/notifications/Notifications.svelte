5fcd2c3bc599724871cdf56a71faea71d574e04c
Ilya Shevelev <i.shevelev@agisoft.com>
2025-03-22 20:30:20 +0100

    Add loading state and notifications.

---
+++ src/lib/notifications/Notifications.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Notification from "./Notification.svelte"
+
+	let { notifications } = $props();
+</script>
+
+<div class="notifications">
+	{#each notifications as notification, i}
+      <Notification
+      	id={notification.id}
+        text={notification.text}
+      /> 
+    {/each}
+</div>
+
+<style>
+	.notifications {
+		position: absolute;
+	    top: 0px;
+	    z-index: 2;
+	    width: 100%;
+	    display: flex;
+	    flex-direction: column;
+	    justify-content: center;
+	    gap: 8px;
+	    margin-block: 16px;
+	}
+</style>
